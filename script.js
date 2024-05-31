44b3242009ac31043006dda12127de027933241c
soutolucianab <163914477+soutolucianab@users.noreply.github.com>
2024-05-31 17:51:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('consulta-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Formulario enviado con éxito. Nos pondremos en contacto con usted pronto.');
+});
